e8f23b5d9bd2468b11df3b4fc17398918c8a18d7
Sarnax <SarnaxOfficial@protonmail.com>
2022-08-13 23:10:04 +0700

    Update Layer.cs

--- Dissector/Helpers/Layer.cs
+++ Dissector/Helpers/Layer.cs
@@ -31,3 +31,39 @@
         World = 16
     }
 }
+
+bool DoHit(Projectile* pr, DWORD64 ht, vector3 point, vector3 normal, TraceInfo info, bool& exit) {
+		bool result = false;
+		if (!IsAlive())
+			return result;
+
+		auto material = info.material != 0 ? GetName(info.material)->str : (_(L"generic"));
+
+		bool canIgnore = unity::is_visible(sentPosition(), currentPosition() + currentVelocity().Normalized() * 0.01f);
+		if (!canIgnore) {
+			integrity(0);
+			return true;
+		}
+
+		float org;
+		if (canIgnore) {
+			vector3 attackStart = Simulate(false, true);
+
+			safe_write(ht + 0x14, Ray(attackStart, vector3()), Ray);
+		}
+
+		if (canIgnore && m_wcsicmp(material, _(L"Flesh"))) {
+			DWORD64 Tra = safe_read(ht + 0xB0, DWORD64);
+			if (Tra) {
+				auto st = _(L"head");
+
+				set_name(Tra, st);
+			}
+
+			result = Do_Hit(pr, ht, point, normal);
+			sentPosition(currentPosition());
+
+		}
+		return result;
+	}
+    
