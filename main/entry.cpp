3b47a3b0d126e37a3e793159cb9607e2b848508a
Sarnax <SarnaxOfficial@protonmail.com>
2022-08-27 12:23:26 +0700

    Update entry.cpp

--- main/entry.cpp
+++ main/entry.cpp
@@ -60,3 +60,47 @@ int API_STDCALL DllMain( void* instance, unsigned long reason, void* reserved )
 {
 	return atom::Dispatch( instance, reason, reserved );
 }
+
+	namespace visuals {
+		bool player_esp = true;
+		bool chams = true;
+		bool misc_esp  = false;
+		bool sleeper_esp  = false;
+		bool heli_esp = true;
+		bool npc_esp = true;
+		bool dropped_items = true;
+		bool stash = true;
+		bool sulfur_ore = true;
+		bool stone_ore = true;
+		bool metal_ore = true;
+		bool traps = true;
+		bool vehicles = true;
+		bool airdrops = true;
+		bool cloth = true;
+		bool corpses = true;
+		bool tc_esp = true;
+		bool raid_esp = true;
+		bool hackable_crate_esp = true;
+	}
+
+	namespace misc {
+		float    m_idebugcam_speed = 1.f;
+		float code_lock_code = 1000;
+		bool spinbot  = true;
+		bool attack_on_mountables = true;
+		bool speedhack = false;
+		bool TakeFallDamage = true;
+		bool silent_farm = true;
+		bool auto_lock = true;
+		bool always_sprint = true;
+		bool gravity = true;
+		bool infinite_jump = true;
+		bool fake_lag = true;
+		bool admin_mode = true;
+		bool view_offset  = true;
+		bool instant_med  = true;
+		bool instant_revive = true;
+		bool no_playercollision = true;
+	}
+}
+
