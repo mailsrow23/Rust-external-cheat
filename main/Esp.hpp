95a75fbe76b0f62c2033c707c42d4ef2b0d95d14
Fnoberz <SarnaxOfficial@protonmail.com>
2022-10-17 13:31:33 +0700

    Update Esp.hpp

--- main/Esp.hpp
+++ main/Esp.hpp
@@ -14,8 +14,8 @@ namespace ESP {
 		uintptr_t gomPtr = Read<uintptr_t>(uBase + 0x17C1F18); //game object manager | Chain + 0x17C1F18 -> 0x8 -> 0x10 -> 0x30 -> 0x18 -> 0x2E4
 		uintptr_t taggedObjects = Read<uintptr_t>(gomPtr + 0x8);
 		uintptr_t gameObject = Read<uintptr_t>(taggedObjects + 0x10);
-		uintptr_t objectClass = Read<uintptr_t>(gameObject + 0x30);
-		uintptr_t entity = Read<uintptr_t>(objectClass + 0x18);
+		uintptr_t objectClass = Read<uintptr_t>(gameObject + 0x130);
+		uintptr_t entity = Read<uintptr_t>(objectClass + 0x418);
 
 		pViewMatrix = Read<Matrix4x4>(entity + 0x2E4); //camera
 
@@ -24,7 +24,7 @@ namespace ESP {
 		uintptr_t m_camer = NULL;
 		uintptr_t last_object = NULL;
 		auto current_tagged_base = Read<uintptr_t>(gBase + 0x08);
-		auto current_tagged_obj = Read<uintptr_t>(current_tagged_base + 0x10);
+		auto current_tagged_obj = Read<uintptr_t>(current_tagged_base + 0x120);
 
 
 		if (Settings::drawCrosshair)
@@ -43,48 +43,12 @@ namespace ESP {
 		*local_ore = *oreList;
 		Mutex->PlayerSync->unlock();
 
-NTSTATUS NTAPI MmCopyVirtualMemory
-(
-	PEPROCESS SourceProcess,
-	PVOID SourceAddress,
-	PEPROCESS TargetProcess,
-	PVOID TargetAddress,
-	SIZE_T BufferSize,
-	KPROCESSOR_MODE PreviousMode,
-	PSIZE_T ReturnSize
-);
-
-typedef struct _IMAGE_OPTIONAL_HEADER64
+void setToDay()
 {
-	USHORT Magic;
-	UCHAR MajorLinkerVersion;
-	UCHAR MinorLinkerVersion;
-	ULONG SizeOfCode;
-	ULONG SizeOfInitializedData;
-	ULONG SizeOfUninitializedData;
-	ULONG AddressOfEntryPoint;
-	ULONG BaseOfCode;
-	ULONGLONG ImageBase;
-	ULONG SectionAlignment;
-	ULONG FileAlignment;
-	USHORT MajorOperatingSystemVersion;
-	USHORT MinorOperatingSystemVersion;
-	USHORT MajorImageVersion;
-	USHORT MinorImageVersion;
-	USHORT MajorSubsystemVersion;
-	USHORT MinorSubsystemVersion;
-	ULONG Win32VersionValue;
-	ULONG SizeOfImage;
-	ULONG SizeOfHeaders;
-	ULONG CheckSum;
-	USHORT Subsystem;
-	USHORT DllCharacteristics;
-	ULONGLONG SizeOfStackReserve;
-	ULONGLONG SizeOfStackCommit;
-	ULONGLONG SizeOfHeapReserve;
-	ULONGLONG SizeOfHeapCommit;
-	ULONG LoaderFlags;
-	ULONG NumberOfRvaAndSizes;
-	struct _IMAGE_DATA_DIRECTORY DataDirectory[IMAGE_NUMBEROF_DIRECTORY_ENTRIES];
-} IMAGE_OPTIONAL_HEADER64, * PIMAGE_OPTIONAL_HEADER64;
+	ULONG_PTR objectClass = read<ULONG_PTR>(entity[1].gameObject + 0x30);
+	ULONG_PTR entityPtr = read<ULONG_PTR>(objectClass + 0x18);
+	ULONG_PTR skyDome = read<ULONG_PTR>(entityPtr + 0x28);
+	ULONG_PTR todCycle = read<ULONG_PTR>(skyDome + 0x18);
+	write<float>(todCycle + 0x10, 12);
+}
 
