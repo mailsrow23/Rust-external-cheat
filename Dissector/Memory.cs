c6c43cbe4946dc7413fe465358ec9dfefa762097
Slackes <SarnaxOfficial@protonmail.com>
2022-12-21 06:45:06 +0700

    Update Memory.cs

--- Dissector/Memory.cs
+++ Dissector/Memory.cs
@@ -1,77 +1,67 @@
-﻿using Binarysharp.MemoryManagement.Internals;
-using Dissector.Helpers;
 using System;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Diagnostics;
 
 namespace covet.cc.Memory
 {
     class Memory
     {
-        public static Kernel kernel;
+        private static readonly string WindowName = "Rust";
+        private static Process process;
+        private static SafeHandle processHandle;
+        private static IntPtr unityPlayer;
 
-        public static MemoryModule Mem = new MemoryModule("RustClient");
+        private static readonly MemoryModule Mem = new MemoryModule("RustClient");
 
-        public class Memory
+        [StructLayout(LayoutKind.Sequential)]
+        private struct Memory
         {
-            public static pad = new string(new char[0x10]);
-            public int strLength = new int();
-            public string str = new string(new char[256]);
+            public string Pad { get; } = new string('\0', 0x10);
+            public int StrLength { get; set; }
+            public string Str { get; set; } = new string('\0', 256);
         }
 
-
-        public struct RECT
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
         {
-            public int left, top, right, bottom;
+            public int Left;
+            public int Top;
+            public int Right;
+            public int Bottom;
         }
-        public static string WindName = "Rust";
-
-        [DllImport("user32.dll")]
-        public static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
         [DllImport("user32.dll", SetLastError = true)]
-        public static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+        private static extern int SetWindowLong(IntPtr hWnd, int nIndex, int dwNewLong);
 
         [DllImport("user32.dll", SetLastError = true)]
-        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        private static extern int GetWindowLong(IntPtr hWnd, int nIndex);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
 
         [DllImport("user32.dll", SetLastError = true)]
         [return: MarshalAs(UnmanagedType.Bool)]
-        public static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
+        private static extern bool GetWindowRect(IntPtr hwnd, out RECT lpRect);
 
         [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
+        private static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData, int dwExtraInfo);
 
         [DllImport("User32.dll")]
-        public static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
-
-        [DllImport("kernel32.dll")]
-        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
-
-        [DllImport("kernel32.dll")]
-        public static extern bool ReadProcessMemory(int hProcess, int lpBaseAddress, byte[] buffer, int size, ref int lpNumberOfBytesRead);
-
-        [DllImport("kernel32.dll")]
-        private static extern bool WriteProcessMemory(int hProcess, int lpBaseAddress, byte[] buffer, int size, out int lpNumberOfBytesWritten);
-
-        [DllImport("user32.dll")]
-        public static extern short GetAsyncKeyState(int vKey);
-
-        public static Process Process;
-        public static IntPtr ProcessHandle;
-        public static IntPtr UnityPlayer;
-        public static int m_iBytesRead = 0;
-        public static int m_iBytesWrite = 0;
+        private static extern short GetAsyncKeyState(System.Windows.Forms.Keys vKey);
 
         [DllImport("kernel32.dll", SetLastError = true)]
-        public static extern bool ReadProcessMemory(IntPtr hProcess, long lpBaseAddress, [In, Out] byte[] lpBuffer, ulong dwSize, out IntPtr lpNumberOfBytesRead);
+        private static extern bool ReadProcessMemory(IntPtr hProcess, long lpBaseAddress, [In, Out] byte[] lpBuffer, ulong dwSize, out IntPtr lpNumberOfBytesRead);
 
+        [DllImport("kernel32.dll", SetLastError = true)]
+        private static extern bool WriteProcessMemory(IntPtr hProcess, long lpBaseAddress, byte[] buffer, ulong size, out IntPtr lpNumberOfBytesWritten);
 
-        public static T ReadMemory<T>(int Adress) where T : struct
+        public static T ReadMemory<T>(long address) where T : struct
         {
-            int ByteSize = Marshal.SizeOf(typeof(T));
-            byte[] buffer = new byte[ByteSize];
-            ReadProcessMemory((int)ProcessHandle, Adress, buffer, buffer.Length, ref m_iBytesRead);
-
+            int structSize = Marshal.SizeOf<T>();
+            byte[] buffer = new byte[structSize];
+            ReadProcessMemory
+                
             return ByteArrayToStructure<T>(buffer);
         }
 
