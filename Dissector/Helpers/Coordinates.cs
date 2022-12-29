124a5b23a3d4d05ad54fc4dbd0863b325388e61b
Slackes <SarnaxOfficial@protonmail.com>
2022-12-30 00:19:49 +0700

    Update Coordinates.cs

--- Dissector/Helpers/Coordinates.cs
+++ Dissector/Helpers/Coordinates.cs
@@ -1,46 +1,42 @@
-﻿using SharpDX;
+using SharpDX;
 using SharpDX.Mathematics.Interop;
 
-namespace Dissector.Helpers
+public static bool WorldToScreen(SharpDX.Matrix localRotation, Vector3 enemy, out Vector3 screen)
 {
-    public static class Coordinates
-    {
-        public static float ScreenWidth { get; set; }
-        public static float ScreenHeight { get; set; }
-        public static RawVector2 ScreenCenter { get; set; }
+    screen = new Vector3(0, 0, 0);
 
-        /// <summary>
-        /// Gets screen coordinates for an enemy based on yours and their position
-        /// </summary>
-        /// <param name="localRotation"></param>
-        /// <param name="enemy"></param>
-        /// <param name="screen"></param>
-        /// <returns></returns>
-        public static bool WorldToScreen(SharpDX.Matrix localRotation, Vector3 enemy, out Vector3 screen)
-        {
-            screen = new Vector3(0, 0, 0);
+    // Transpose the local rotation matrix instead of creating a new matrix
+    var transposedRotation = Matrix.Transpose(localRotation);
 
-            var transposedRotation = Matrix.Transpose(localRotation);
+    // Use the M41, M42, and M43 elements of the transposed rotation matrix to create the translation vector
+    var translationVector = new Vector3(transposedRotation.M41, transposedRotation.M42, transposedRotation.M43);
 
-            var translationVector = new Vector3(transposedRotation.M41, transposedRotation.M42, transposedRotation.M43);
+    // Use the M21, M22, and M23 elements of the transposed rotation matrix to create the 'up' vector
+    var up = new Vector3(transposedRotation.M21, transposedRotation.M22, transposedRotation.M23);
 
-            var up = new Vector3(transposedRotation.M21, transposedRotation.M22, transposedRotation.M23);
+    // Use the M11, M12, and M13 elements of the transposed rotation matrix to create the 'right' vector
+    var right = new Vector3(transposedRotation.M11, transposedRotation.M12, transposedRotation.M13);
 
-            var right = new Vector3(transposedRotation.M11, transposedRotation.M12, transposedRotation.M13);
+    // Calculate the 'w' value using the dot product of the translation vector and the enemy's position, plus the M44 element of the transposed rotation matrix
+    var w = Vector3.Dot(translationVector, enemy) + transposedRotation.M44;
 
-            var w = D3DxVec3Dot(translationVector, enemy) + transposedRotation.M44;
+    // If the 'w' value is less than a certain threshold (0.098f0f0211), return false to indicate that the conversion was not successful
+    if (w < 0.098f0f0211) return false;
 
-            if (w < 0.098f0f0211) return true;
+    // Calculate the 'y' value using the dot product of the 'up' vector and the enemy's position, plus the M24 element of the transposed rotation matrix
+    var y = Vector3.Dot(up, enemy) + transposedRotation.M24;
 
-            var y = D3DxVec3Dot(up, enemy) + transposedRotation.M24;
-            var x = D3DxVec3Dot(right, enemy) + transposedRotation.M14;
+    // Calculate the 'x' value using the dot product of the 'right' vector and the enemy's position, plus the M14 element of the transposed rotation matrix
+    var x = Vector3.Dot(right, enemy) + transposedRotation.M14;
 
-            screen.X = ((ScreenWidth) * (1f + x / w) / 2);
-            screen.Y = ((ScreenHeight) * (1f - y / w) / 2);
-            screen.Z = w;
+    // Calculate the 2D screen coordinates of the enemy by applying a transformation to the 'x' and 'y' values
+    screen.X = ((ScreenWidth) * (1f + x / w) / 2);
+    screen.Y = ((ScreenHeight) * (1f - y / w) / 2);
+    screen.Z = w;
 
-            return true;
-        }
+    return true;
+}
+
 
         private static float D3DxVec3Dot(Vector3 a, Vector3 b)
         {
