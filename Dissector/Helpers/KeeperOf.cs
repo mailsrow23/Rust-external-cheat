97792d8bf4a47188311eee4a8cbda9a70fdfc2bf
Slackes <SarnaxOfficial@protonmail.com>
2023-01-28 01:17:39 +0700

    Update KeeperOf.cs

--- Dissector/Helpers/KeeperOf.cs
+++ Dissector/Helpers/KeeperOf.cs
@@ -1,10 +1,33 @@
-﻿using System;
+using System;
+using Dissector.Memory;
 
 namespace Dissector.Helpers
 {
     public static class KeeperOf
     {
-        public static Memory Memory { get; set; }
-        public static IntPtr BaseNetworkableAddress { get; set; }
+        private static Memory _memory;
+        private static IntPtr _baseNetworkableAddress;
+
+        public static Memory Memory
+        {
+            get
+            {
+                if(_memory == null)
+                    throw new Exception("Memory object is not initialized.");
+                return _memory;
+            }
+            set => _memory = value;
+        }
+
+        public static IntPtr BaseNetworkableAddress
+        {
+            get
+            {
+                if(_baseNetworkableAddress == IntPtr.Zero)
+                    throw new Exception("BaseNetworkableAddress is not initialized.");
+                return _baseNetworkableAddress;
+            }
+            set => _baseNetworkableAddress = value;
+        }
     }
 }
