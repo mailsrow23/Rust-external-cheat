455806ab5426de49520699f539f99e49997de6bb
Serups <SarnaxOfficial@protonmail.com>
2023-03-18 00:57:09 +0700

    Assembly Attribute Summary

--- LoaderUI/Properties/AssemblyInfo.cs
+++ LoaderUI/Properties/AssemblyInfo.cs
@@ -1,21 +1,10 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("RyuzakiUI")]
 [assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("RyuzakiUI")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
 [assembly: Guid("e7a28e2e-6be4-4d6f-b6e8-d78545d9bbd2")]
-
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
